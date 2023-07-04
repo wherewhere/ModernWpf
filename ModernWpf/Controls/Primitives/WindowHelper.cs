e577ed998253cd47cb78b14156946b3f8503556e
KirsebaerQAQ <lizijie1@kingsoft.com>
2023-07-04 15:55:31 +0800

    添加UseMicaBackdrop功能

--- ModernWpf/Controls/Primitives/WindowHelper.cs
+++ ModernWpf/Controls/Primitives/WindowHelper.cs
@@ -172,6 +172,72 @@ namespace ModernWpf.Controls.Primitives
 
         #endregion
 
+        #region UseMicaBackdrop
+        public class ParameterTypes
+        {
+            /*
+            [Flags]
+            enum DWM_SYSTEMBACKDROP_TYPE
+            {
+                DWMSBT_MAINWINDOW = 2, // Mica
+                DWMSBT_TRANSIENTWINDOW = 3, // Acrylic
+                DWMSBT_TABBEDWINDOW = 4 // Tabbed
+            }
+            */
+
+            [Flags]
+            public enum DWMWINDOWATTRIBUTE
+            {
+                DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
+                DWMWA_SYSTEMBACKDROP_TYPE = 38
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct MARGINS
+            {
+                public int cxLeftWidth;      // width of left border that retains its size
+                public int cxRightWidth;     // width of right border that retains its size
+                public int cyTopHeight;      // height of top border that retains its size
+                public int cyBottomHeight;   // height of bottom border that retains its size
+            };
+        }
+
+        public static class Methods
+        {
+            [DllImport("DwmApi.dll")]
+            static extern int DwmExtendFrameIntoClientArea(
+                IntPtr hwnd,
+                ref ParameterTypes.MARGINS pMarInset);
+
+            [DllImport("dwmapi.dll")]
+            static extern int DwmSetWindowAttribute(IntPtr hwnd, ParameterTypes.DWMWINDOWATTRIBUTE dwAttribute, ref int pvAttribute, int cbAttribute);
+
+            public static int ExtendFrame(IntPtr hwnd, ParameterTypes.MARGINS margins)
+                => DwmExtendFrameIntoClientArea(hwnd, ref margins);
+
+            public static int SetWindowAttribute(IntPtr hwnd, ParameterTypes.DWMWINDOWATTRIBUTE attribute, int parameter)
+                => DwmSetWindowAttribute(hwnd, attribute, ref parameter, Marshal.SizeOf<int>());
+        }
+        public static void SetUseMicaBackdrop(Window window)
+        {
+            Methods.SetWindowAttribute(
+                new WindowInteropHelper(window).Handle,
+                ParameterTypes.DWMWINDOWATTRIBUTE.DWMWA_SYSTEMBACKDROP_TYPE,
+                2);
+            RefreshDarkMode(window);
+            ThemeManager.Current.ActualApplicationThemeChanged += (s, ev) => RefreshDarkMode(window);
+        }
+        private static void RefreshDarkMode(Window window)
+        {
+            var isDark = ThemeManager.Current.ActualApplicationTheme == ApplicationTheme.Dark;
+            int flag = isDark ? 1 : 0;
+            Methods.SetWindowAttribute(
+                new WindowInteropHelper(window).Handle,
+                ParameterTypes.DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE,
+                flag);
+        }
+        #endregion
+
         #region SystemBackdropType
 
         public static readonly DependencyProperty SystemBackdropTypeProperty =
